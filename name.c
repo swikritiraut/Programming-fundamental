7e3b5d67d0b6736008da65516e8e4c225389ab73
swikritiraut <swikriti.raut22@gmail.com>
2016-11-27 20:58:31 -0800

    Add files via upload

---
+++ name.c
@@ -0,0 +1,14 @@
+/*
+Student Name:Swikriti Raut
+Subject:programming fundamental
+Roll:
+Lab sheet No:1
+Program:wap to print name
+Date:28 Nov,2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main() {
+printf("my name is :swikriti");
+return(0);
+}

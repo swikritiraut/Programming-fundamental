d01cba3c9398af6be0b8eb5282a95a6a7d751308
swikritiraut <swikriti.raut22@gmail.com>
2016-11-27 21:48:10 -0800

    Add files via upload

--- name.c
+++ name.c
@@ -3,12 +3,20 @@ Student Name:Swikriti Raut
 Subject:programming fundamental
 Roll:
 Lab sheet No:1
-Program:wap to print name
+Program:wap to print number
 Date:28 Nov,2016
 */
 #include<stdio.h>
-#include<conio.h>
+
 int main() {
-printf("my name is :swikriti");
-return(0);
+int a,b;
+printf("enter a first number : \n ");
+scanf("%d", &a);
+printf("enter a second number is :\n");
+scanf("%d", &b);
+printf("the entered  first number : %d\n",a);
+printf("the entered second number : %d",b);
+return 0;
 }
+
+

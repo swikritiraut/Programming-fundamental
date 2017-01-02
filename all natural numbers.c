2b3d27c8e397f7dae12a5d895ec5617ca39dfffd
swikritiraut <swikriti.raut22@gmail.com>
2017-01-02 10:26:49 +0545

    Add files via upload

---
+++ all natural numbers.c
@@ -0,0 +1,26 @@
+/*
+name:swikriti ray
+subject:programming fundamental
+lab sheet:14
+date:2jan
+program:wap to print all natural numbers from 1 to n using while loop
+*/
+#include<stdio.h>
+#include<conio.h>
+int main() {
+int a=1,n;
+printf("enter the number");
+scanf("%d",&n);
+while(a<=n)
+{
+    printf("%d",a++);
+
+}
+    return 0;
+}
+
+
+
+
+
+

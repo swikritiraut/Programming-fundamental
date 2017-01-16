37963fb5cf18f5935152ffbe116f21b863cb923e
swikritiraut <swikriti.raut22@gmail.com>
2017-01-16 12:31:24 +0545

    Add files via upload

---
+++ display.c
@@ -0,0 +1,23 @@
+/*
+student name:swikriti ray
+lab sheet:17
+date:16 jan
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i,j,n=1;
+    for(i=1;i<=2;i++)
+    {
+        for(j=1;j<=3;j++)
+    {
+    printf("%d",n);
+
+    }
+    n++;
+    printf("\n");
+    }
+    getch();
+    return 0;
+    }

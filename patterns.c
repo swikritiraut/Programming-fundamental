d92db8ec5841a9e77653b2886c21b6ec8b6f850f
swikritiraut <swikriti.raut22@gmail.com>
2017-01-16 11:25:22 +0545

    Add files via upload

---
+++ patterns.c
@@ -0,0 +1,28 @@
+/*
+student name:swikriti rqay
+lab sheet:17
+program:wap to display the following patterns
+subject:programming fundamental
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i,j,n;
+    for(i=1;i<=5;i++)
+{
+
+
+for(j=1;j<=i;j++)
+{
+    printf("%d",j);
+
+}
+
+printf("\n");
+
+}
+return 0;
+getch();
+}
+

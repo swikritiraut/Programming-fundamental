7717803932fb948136f54aa494705d62a7525fdf
swikritiraut <swikriti.raut22@gmail.com>
2017-01-16 11:24:53 +0545

    Add files via upload

---
+++ name 5 times.c
@@ -0,0 +1,21 @@
+/*
+student name:swikriti ray
+lab sheet:17
+subject:programming fundamental
+program: write a name five times
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i;
+for(i=1;i<=5;i++)
+{
+
+        printf("ray\n");
+
+
+}
+return 0;
+getch();
+}

e2a3faf9d04339f3b861072707b654cd782ffa60
swikritiraut <swikriti.raut22@gmail.com>
2017-01-18 12:51:17 +0545

    Add files via upload

---
+++ area diameter circumference.c
@@ -0,0 +1,35 @@
+
+#include<stdio.h>
+#define pi 3.14
+void diameter(float r);
+void area(float r);
+void circumfrence(float r);
+
+int main()
+{
+	float r;
+	printf("enter the value of radious\n:");
+	scanf("%f",&r);
+	diameter(r);
+	area(r);
+	circumfrence(r);
+	return 0;
+}
+void diameter(float r)
+{
+	float d;
+	d=2*r;
+	printf("\ndiameter =%0.2f",d);
+}
+void area(float r)
+{
+	float a;
+	a=pi*r*r;
+	printf("\narea of circle=%0.2f",a);
+}
+void circumfrence(float r)
+{
+	float c;
+	c=2*pi*r;
+	printf("\ncircumfreence of circle=%0.2f",c);
+}

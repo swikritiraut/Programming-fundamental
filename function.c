bc4d5bff8bd9c53299334c4caa418ad87250b7ec
swikritiraut <swikriti.raut22@gmail.com>
2017-01-18 11:26:37 +0545

    Add files via upload

---
+++ function.c
@@ -0,0 +1,21 @@
+/*
+student name:swikriti ray
+lab sheet:20
+subject:programming fundamental
+program:Wap to find cube of any numbers using function
+date:18 jan
+*/
+#include<stdio.h>
+void function(int num);
+int main()
+{
+	int num;
+	printf("enter a number");
+	scanf("%d",&num);
+	function1(num);
+	return (0);
+}
+void function1(int num)
+{
+	printf("the number is %d",num);
+}

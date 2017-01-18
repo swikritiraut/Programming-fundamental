56e86a8b15d457edd1f0a9c70a4217b6beb812fb
swikritiraut <swikriti.raut22@gmail.com>
2017-01-18 11:06:58 +0545

    Add files via upload

---
+++ length breath  area of rectangle using function.c
@@ -0,0 +1,25 @@
+/*
+student name:swikriti ray
+lab sheet:20
+subject:programming fundamental
+program:Wap to find cube of any numbers using function
+date:18 jan
+*/
+#include<stdio.h>
+void area();
+int main()
+{ 
+	area();
+	return 0;
+}
+void area()
+{
+	int l,b,area;
+	printf("enter the length");
+	scanf("%d",&l);
+	printf("enter the breadth");
+	scanf("%d",&b);
+	area=l*b;
+	printf("area of rectangle is :%d",area);
+}
+	

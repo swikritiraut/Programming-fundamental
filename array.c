39c2288a9e8689da65f09bf71d1c44db6f9aab0d
swikritiraut <swikriti.raut22@gmail.com>
2017-02-01 10:44:24 +0545

    Add files via upload

---
+++ array.c
@@ -0,0 +1,22 @@
+/*
+ student name:Santosh darlami
+ lab sheet   :
+ program     :to read and print element of any array
+ date:22 jan
+ */
+ #include<stdio.h>
+ int main()
+ {
+	 int a[5],i;
+	 for(i=0;i<5;i++)
+	 {
+		 printf("enter any no.\n:");
+		 scanf("%d",&a[i]);
+	}
+	for(i=0;i<5;i++)
+	 {
+		 printf("\n %d",a[i]);
+	}
+return 0;
+			 
+		 }

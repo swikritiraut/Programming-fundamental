cfbddda6a959cc7c4ab4fab72575933228e4027b
swikritiraut <swikriti.raut22@gmail.com>
2017-02-01 11:12:53 +0545

    Add files via upload

---
+++ array2nd.c
@@ -0,0 +1,22 @@
+/*
+student name:swikritin ray
+subjct:programming fundamental
+lab sheet:24
+date:1 Feb
+program:
+*/
+#include<stdio.h>
+int main()
+{
+	int a[10],i;
+	for(i=0;i<10;i++)
+	{
+		printf("enter a number:\n");
+		scanf("%d",&a [i]);
+	}
+	for(i=0;i<10;i++)
+	{
+		printf("a[%d]=%d\n",i,a[i]);
+	}
+	return 0;
+}
